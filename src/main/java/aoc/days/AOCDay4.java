df8f353580530cd1a6b0f7c164f23ea922f1a892
TT54 <tt54.of@gmail.com>
2024-12-04 17:59:26 +0100

    Day 4 - Cleaning

--- src/main/java/aoc/days/AOCDay4.java
+++ src/main/java/aoc/days/AOCDay4.java
@@ -2,80 +2,71 @@ package aoc.days;
 
 import aoc.utils.DataUtils;
 
-import javax.swing.*;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AOCDay4 implements AbstractDay{
 
     private List<String> input;
+    private final int[][] searchDirections = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+    private final char[] searchedChars = new char[] {'X', 'M', 'A', 'S'};
+    private final int[][] simplifiedSearchDirections = new int[][]{{1, 1}, {1, -1}};
+
+    private int countXMAS(int row, int column){
+        int count = 0;
+        for(int[] direction : searchDirections) {
+            int currentRow = row, currentColumn = column;
+            for (char c : searchedChars) {
+                if(currentRow < 0 || currentColumn < 0 || currentRow >= input.size() || currentColumn >= input.get(currentRow).length()
+                        || input.get(currentRow).charAt(currentColumn) != c){
+                    break;
+                }
+
+                if(c == 'S') count++;
+                currentRow += direction[0];
+                currentColumn += direction[1];
+            }
+        }
+        return count;
+    }
 
     @Override
     public int getPart1Result(String dataPath) {
         this.readDatas(dataPath);
 
-        final int[][] searchDirections = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-        final char[] searchedChars = new char[] {'X', 'M', 'A', 'S'};
-
         int count = 0;
         for(int i = 0; i < input.size(); i++){
             for(int j = 0; j < input.get(i).length(); j++){
-                char searchedChar = searchedChars[0];
-                char c = input.get(i).charAt(j);
-
-                if(c == searchedChar){
-                    for(int[] direction : searchDirections){
-                        int row = i;
-                        int column = j;
-                        boolean found = true;
-
-                        for(int k = 1; k < searchedChars.length; k++){
-                            row += direction[0];
-                            column += direction[1];
-
-                            if(row < 0 || column < 0 || row >= input.size() || column >= input.get(row).length() || input.get(row).charAt(column) != searchedChars[k]){
-                                found = false;
-                                break;
-                            }
-                        }
-
-                        if(found){
-                            count++;
-                        }
-                    }
-                }
+                count += countXMAS(i, j);
             }
         }
 
         return count;
     }
 
+    public boolean isThereX_MAS(int row, int column){
+        try {
+            if(input.get(row).charAt(column) != 'A') return false;
+            int masFound = 0;
+            for(int[] direction : simplifiedSearchDirections){
+                if(input.get(row + direction[0]).charAt(column + direction[1]) == 'M' && input.get(row - direction[0]).charAt(column - direction[1]) == 'S'
+                        || input.get(row + direction[0]).charAt(column + direction[1]) == 'S' && input.get(row - direction[0]).charAt(column - direction[1]) == 'M'){
+                    masFound++;
+                }
+            }
+            return masFound == 2;
+        } catch (IndexOutOfBoundsException ignored){
+            return false;
+        }
+    }
+
     @Override
     public int getPart2Result(String dataPath) {
         this.readDatas(dataPath);
 
-        final int[][] simplifiedSearchDirections = new int[][]{{1, 1}, {1, -1}};
-
         int count = 0;
         for(int i = 0; i < input.size(); i++){
             for(int j = 0; j < input.get(i).length(); j++){
-                char c = input.get(i).charAt(j);
-
-                try {
-                    if(c == 'A'){
-                        int masFound = 0;
-                        for(int[] direction : simplifiedSearchDirections){
-                            if(input.get(i + direction[0]).charAt(j + direction[1]) == 'M' && input.get(i - direction[0]).charAt(j - direction[1]) == 'S'
-                            || input.get(i + direction[0]).charAt(j + direction[1]) == 'S' && input.get(i - direction[0]).charAt(j - direction[1]) == 'M'){
-                                masFound++;
-                            }
-                        }
-                        if(masFound == 2){
-                            count++;
-                        }
-                    }
-                } catch (IndexOutOfBoundsException ignored){}
+                if(isThereX_MAS(i, j)) count++;
             }
         }
 
